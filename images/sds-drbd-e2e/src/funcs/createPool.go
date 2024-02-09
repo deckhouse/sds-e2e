08834ff185996fd2b866a854285951eda7769674
v.oleynikov <vasily.oleynikov@flant.com>
2024-02-09 16:57:16 +0300

    Initial commit

--- images/sds-drbd-e2e/src/funcs/createPool.go
+++ images/sds-drbd-e2e/src/funcs/createPool.go
@@ -1,33 +1,14 @@
-package tests
+package funcs
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
-	"path/filepath"
-	"testing"
 )
-
-func CreatePoolTest(t *testing.T) {
-	kubeconfig := flag.String("kubeconfig", filepath.Join("/app", "kube.config.internal"), "(optional) absolute path to the kubeconfig file")
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	dynamicClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	CreatePools(*dynamicClient)
-}
 
 func CreatePools(client dynamic.DynamicClient) {
 	bdRes := schema.GroupVersionResource{Group: "storage.deckhouse.io", Version: "v1alpha1", Resource: "blockdevices"}
