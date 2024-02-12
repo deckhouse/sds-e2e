1d9ee5eb94a51ca349b49986bbc8e725661d05b3
v.oleynikov <vasily.oleynikov@flant.com>
2024-02-12 06:57:00 +0300

    Initial

--- images/sds-drbd-e2e/src/tests/02_create_sts_with_logs_test.go
+++ images/sds-drbd-e2e/src/tests/02_create_sts_with_logs_test.go
@@ -1,33 +1,10 @@
 package test
 
 import (
-	"flag"
-	"fmt"
 	"gocontainer/funcs"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
 func TestCreateStsLogs(t *testing.T) {
-	kubeconfigPath := os.Getenv("kubeconfig")
-	fmt.Printf(kubeconfigPath)
-	if kubeconfigPath == "" {
-		kubeconfigPath = filepath.Join("/app", "kube.config.internal")
-	}
-
-	kubeconfig := flag.String("kubeconfig", kubeconfigPath, "(optional) absolute path to the kubeconfig file")
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
-
 	funcs.CreateLogSts(*clientset)
 }
