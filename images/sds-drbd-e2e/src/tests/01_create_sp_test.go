25ea7f9bc04bd61587c71fa93494815f5feae8b7
v.oleynikov <vasily.oleynikov@flant.com>
2024-02-12 06:58:16 +0300

    Initial

--- images/sds-drbd-e2e/src/tests/01_create_sp_test.go
+++ images/sds-drbd-e2e/src/tests/01_create_sp_test.go
@@ -1,10 +1,34 @@
 package test
 
 import (
+	"flag"
+	"fmt"
 	"gocontainer/funcs"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestCreatePool(t *testing.T) {
+	kubeconfigPath := os.Getenv("kubeconfig")
+
+	fmt.Printf(kubeconfigPath)
+	if kubeconfigPath == "" {
+		kubeconfigPath = filepath.Join("/app", "kube.config.internal")
+	}
+
+	kubeconfig := flag.String("kubeconfig", kubeconfigPath, "(optional) absolute path to the kubeconfig file")
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	funcs.CreatePools(*dynamicClient)
 }
