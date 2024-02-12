21c8672ca4ee6a467c4a924b6d4a2c8fdd6f31e9
v.oleynikov <vasily.oleynikov@flant.com>
2024-02-12 06:40:23 +0300

    Initial

--- images/sds-drbd-e2e/src/tests/01_create_sp_test.go
+++ images/sds-drbd-e2e/src/tests/01_create_sp_test.go
@@ -14,7 +14,11 @@ import (
 func TestCreatePool(t *testing.T) {
 	kubeconfigPath := os.Getenv("kubeconfig")
 	fmt.Printf(kubeconfigPath)
-	kubeconfig := flag.String("kubeconfig", filepath.Join("/app", "kube.config.internal"), "(optional) absolute path to the kubeconfig file")
+	if kubeconfigPath == "" {
+		kubeconfigPath = filepath.Join("/app", "kube.config.internal")
+	}
+
+	kubeconfig := flag.String("kubeconfig", kubeconfigPath, "(optional) absolute path to the kubeconfig file")
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
