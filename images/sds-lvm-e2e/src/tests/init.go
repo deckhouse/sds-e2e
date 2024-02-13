f1d6e87ef600788176d271c7be7cea3c30743210
v.oleynikov <vasily.oleynikov@flant.com>
2024-02-13 11:03:54 +0300

    Initial
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/sds-lvm-e2e/src/tests/init.go
+++ images/sds-lvm-e2e/src/tests/init.go
@@ -1,26 +1,41 @@
 package test
 
 import (
+	"fmt"
 	v1 "k8s.io/api/core/v1"
 	sv1 "k8s.io/api/storage/v1"
 	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apiruntime "k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"path/filepath"
 	"sds-lvm-e2e/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 func NewKubeClient() (client.Client, error) {
-	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-		clientcmd.NewDefaultClientConfigLoadingRules(),
-		&clientcmd.ConfigOverrides{},
-	)
+	//clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+	//	clientcmd.NewDefaultClientConfigLoadingRules(),
+	//	&clientcmd.ConfigOverrides{},
+	//)
+	//
+	//config, err := clientConfig.ClientConfig()
+	//if err != nil {
+	//	return nil, err
+	//}
 
-	config, err := clientConfig.ClientConfig()
+	kubeconfigPath := os.Getenv("kubeconfig")
+
+	fmt.Printf(kubeconfigPath)
+	if kubeconfigPath == "" {
+		kubeconfigPath = filepath.Join("/app", "kube.config.internal")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
-		return nil, err
+		panic(err.Error())
 	}
 
 	var (
