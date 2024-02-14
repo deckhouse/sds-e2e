0eb8180ca93195a2cba42ccf8f9c0211e2e0a48d
v.oleynikov <vasily.oleynikov@flant.com>
2024-02-14 09:23:31 +0300

    Initial
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/sds-drbd-e2e/src/funcs/drbdstorage_pool_class.go
+++ images/sds-drbd-e2e/src/funcs/drbdstorage_pool_class.go
@@ -51,7 +51,9 @@ func CreatePools(ctx context.Context, cl client.Client) error {
 
 	err := CreateDrbdStoragePool(ctx, cl, "data", lvmVolumeGroupList)
 	if err != nil {
-		return err
+		if err.Error() != "Pool creation error drbdstoragepools.storage.deckhouse.io \"data\" already exists" {
+			return err
+		}
 	}
 
 	err = CreateDrbdStorageClass(ctx, cl, "linstor-r1", "None", false)
