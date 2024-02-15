5cc104a85bbc8e849e2939936ecefc0e69cde3ea
Alexandr Ohrimenko <alexandr.ohrimenko@flant.com>
2024-02-15 11:33:19 +0300

    [CSI] add test check file system and create file
    Signed-off-by: Alexandr Ohrimenko <alexandr.ohrimenko@flant.com>

--- images/sds-lvm-e2e/src/tests/03_case_26_test.go
+++ images/sds-lvm-e2e/src/tests/03_case_26_test.go
@@ -12,7 +12,7 @@ func init() {
 	fmt.Println("Create manual LVG resource vg-data-on-node-worker-1")
 }
 
-func Test_23_24(t *testing.T) {
+func Test_26(t *testing.T) {
 	ctx := context.Background()
 	cl, err := NewKubeClient()
 	if err != nil {
@@ -27,7 +27,7 @@ func Test_23_24(t *testing.T) {
 	t.Log(fmt.Sprintf("pvc=%s created", pvcName))
 
 	command := []string{"/bin/bash"}
-	args := []string{"-c", "df -T | grep '/usr/share/test-data' | grep 'ext4'"}
+	args := []string{"-c", "touch /usr/share/test-data/test.txt"}
 	podName, err := funcs.CreatePod(ctx, cl, "test-pod", pvcName, false, command, args)
 	if err != nil {
 		t.Error(err)
