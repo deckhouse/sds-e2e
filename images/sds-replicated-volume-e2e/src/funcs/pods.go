76f3e806f9941d2b5a6e3faa69d06f18e25bd741
v.oleynikov <vasily.oleynikov@flant.com>
2024-03-05 16:57:28 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

---
+++ images/sds-replicated-volume-e2e/src/funcs/pods.go
@@ -0,0 +1,23 @@
+package funcs
+
+import (
+	"context"
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func ListPods(ctx context.Context, cl client.Client, namespaceName string) error {
+	objs := corev1.PodList{}
+	opts := client.ListOption(&client.ListOptions{Namespace: namespaceName})
+	err := cl.List(ctx, &objs, opts)
+	if err != nil {
+		return err
+	}
+
+	for count, item := range objs.Items {
+		fmt.Printf("%d: #%v", count, item)
+	}
+
+	return nil
+}
---
+++ images/sds-replicated-volume-e2e/src/tests/01_create_sp_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"context"
+	"sds-replicated-volume-e2e/funcs"
+	"testing"
+)
+
+func TestCreatePool(t *testing.T) {
+	ctx := context.Background()
+	cl, err := NewKubeClient()
+	if err != nil {
+		t.Error("kubeclient error", err)
+	}
+
+	err = funcs.CreatePools(ctx, cl)
+	if err != nil {
+		t.Error("Pool creation error", err)
+	}
+}
