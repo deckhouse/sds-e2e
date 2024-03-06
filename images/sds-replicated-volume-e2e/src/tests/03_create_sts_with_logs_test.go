f71251e17f039f8e2c0f750868f390baf7f34339
v.oleynikov <vasily.oleynikov@flant.com>
2024-03-06 11:35:45 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/sds-replicated-volume-e2e/src/tests/03_create_sts_with_logs_test.go
+++ images/sds-replicated-volume-e2e/src/tests/03_create_sts_with_logs_test.go
@@ -13,10 +13,12 @@ func TestCreateStsLogs(t *testing.T) {
 		t.Error("kubeclient error", err)
 	}
 
-	//	err = funcs.CreateLogSts(ctx, cl, "d8-sds-replicated-volume-e2e-test")
-	//	if err != nil {
-	//		t.Error("sts creation error", err)
-	//	}
+	err = funcs.CreateLogSts(ctx, cl, "d8-sds-replicated-volume-e2e-test")
+	if err != nil {
+		t.Error("sts creation error", err)
+	}
 
 	funcs.ListPods(ctx, cl, "")
+
+	funcs.WaitForLogStsPods(ctx, cl, "d8-sds-replicated-volume-e2e-test")
 }
