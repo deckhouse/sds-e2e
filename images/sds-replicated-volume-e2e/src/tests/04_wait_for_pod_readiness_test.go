3b3bd1e75d36cec7b87416c7c607f74483a906a7
v.oleynikov <vasily.oleynikov@flant.com>
2024-03-06 22:17:58 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/sds-replicated-volume-e2e/src/tests/04_wait_for_pod_readiness_test.go
+++ images/sds-replicated-volume-e2e/src/tests/04_wait_for_pod_readiness_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"context"
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
 	"sds-replicated-volume-e2e/funcs"
 	"testing"
 )
@@ -13,8 +15,26 @@ func TestWaitStsPods(t *testing.T) {
 		t.Error("kubeclient error", err)
 	}
 
-	err = funcs.WaitForPodsReadiness(ctx, cl, "d8-sds-replicated-volume-e2e-test")
-	if err != nil {
-		t.Error("Pods waiting error", err)
+	for count := 0; count < 60; count++ {
+		fmt.Printf("Wait for all pods to be ready")
+
+		allPodsReady := true
+		podList, err := funcs.ListPods(ctx, cl, "d8-sds-replicated-volume-e2e-test")
+		if err != nil {
+			t.Error("Pod list error", err)
+		}
+		for _, item := range podList {
+			if item.Phase != corev1.PodRunning {
+				allPodsReady = false
+			}
+		}
+
+		if allPodsReady {
+			break
+		}
+
+		if count == 60 {
+			t.Errorf("Timeout waiting for all pods to be ready")
+		}
 	}
 }
