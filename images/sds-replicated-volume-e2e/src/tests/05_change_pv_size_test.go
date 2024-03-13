349eae3127553183b719cbf18449ff6ee3cc54b6
v.oleynikov <vasily.oleynikov@flant.com>
2024-03-13 19:13:45 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/sds-replicated-volume-e2e/src/tests/05_change_pv_size_test.go
+++ images/sds-replicated-volume-e2e/src/tests/05_change_pv_size_test.go
@@ -32,7 +32,7 @@ func TestChangeStsPvcSize(t *testing.T) {
 			t.Error("PVC size change error", err)
 		}
 		for _, pvc := range pvcList {
-			if pvc.Size != "1153434Ki" {
+			if pvc.Size != "5347737Ki" {
 				allPvcChanged = false
 			}
 		}
