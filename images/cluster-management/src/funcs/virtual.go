c86d185587197c837bcd567233fe47f684b147dd
v.oleynikov <vasily.oleynikov@flant.com>
2024-03-21 15:25:19 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/cluster-management/src/funcs/virtual.go
+++ images/cluster-management/src/funcs/virtual.go
@@ -278,30 +278,10 @@ func CreateVM(ctx context.Context,
 package_update: true
 packages:
 - qemu-guest-agent
-- nginx
-write_files:
-- path: /usr/scripts/genpage_script.sh
-  permissions: "0755"
-  content: |
-	#!/bin/bash
-
-	cat > /var/www/html/index.html<<EOF
-	<!DOCTYPE html>
-	<html>
-	<head>
-	<title>Welcome to $(hostname)<title>
-	</head>
-	<body>
-	<h1>Welcome to nginx on server $(hostname) !</h1>
-	</body>
-	</html>
-	EOF
 runcmd:
 - [ hostnamectl, set-hostname, %s ]
 - [ systemctl, daemon-reload ]
 - [ systemctl, enable, --now, qemu-guest-agent.service ]
-- [ /usr/scripts/genpage_script.sh ]
-- [ systemctl, enable, --now, nginx ]
 user: user
 password: user
 ssh_pwauth: True
