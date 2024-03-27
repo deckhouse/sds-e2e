d97534d9f62702eb74e67b8564223ff2760a6510
v.oleynikov <vasily.oleynikov@flant.com>
2024-03-27 14:32:04 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/cluster-management/src/main.go
+++ images/cluster-management/src/main.go
@@ -60,7 +60,8 @@ func main() {
 	fmt.Printf("err: %v\n", err)
 
 	licenseKey := os.Getenv("licensekey")
-	sshCommand := fmt.Sprintf("sudo docker login -u license-token -p %s dev-registry.deckhouse.io && mkdir -p .ssh && sudo docker run --pull=always -t -v '/home/tfadm/config.yml:/config.yml' -v '/home/tfadm/.ssh/:/tmp/.ssh/' dev-registry.deckhouse.io/sys/deckhouse-oss/install:main dhctl bootstrap --ssh-user=user --ssh-host=10.10.10.180 --ssh-password=user --config=/config.yml", licenseKey)
+	sshCommand := fmt.Sprintf("sudo docker login -u license-token -p %s dev-registry.deckhouse.io", licenseKey)
+	//" && mkdir -p .ssh && sudo docker run --pull=always -t -v '/home/tfadm/config.yml:/config.yml' -v '/home/tfadm/.ssh/:/tmp/.ssh/' dev-registry.deckhouse.io/sys/deckhouse-oss/install:main dhctl bootstrap --ssh-user=user --ssh-host=10.10.10.180 --ssh-password=user --config=/config.yml", licenseKey)
 
 	output, err = funcs.RemoteRun("user", "10.10.10.181", "user", sshCommand)
 	fmt.Printf("output: %v\n", output)
