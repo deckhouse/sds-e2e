38f269c89d9f3d6c941e67c8d4c1e84e58cf01af
v.oleynikov <vasily.oleynikov@flant.com>
2024-03-28 13:50:42 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/cluster-management/src/main.go
+++ images/cluster-management/src/main.go
@@ -20,6 +20,7 @@ import (
 	"cluster-management/funcs"
 	"cluster-management/tests"
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -52,7 +53,11 @@ func main() {
 		fmt.Printf("%s\n", item.Name)
 	}
 
-	funcs.GenerateRSAKeys("./id_rsa_test", "./id_rsa_test.pub")
+	if _, err := os.Stat("/path/to/whatever"); err == nil {
+		fmt.Printf("RSA keys exists")
+	} else if errors.Is(err, os.ErrNotExist) {
+		funcs.GenerateRSAKeys("./id_rsa_test", "./id_rsa_test.pub")
+	}
 
 	sshPubKey, err := os.ReadFile("./id_rsa_test.pub")
 	if err != nil {
@@ -62,22 +67,22 @@ func main() {
 
 	fmt.Printf(sshPubKeyString)
 
-	output, err := funcs.RemoteRun("user", "10.10.10.181", "user", "ls -l /")
-	fmt.Printf("output: %v\n", output)
-	fmt.Printf("err: %v\n", err)
-
-	output, err = funcs.RemoteRun("user", "10.10.10.181", "user", "sudo apt update && sudo apt -y install docker.io")
-	fmt.Printf("output: %v\n", output)
-	fmt.Printf("err: %v\n", err)
-
-	licenseKey := os.Getenv("licensekey")
-	fmt.Printf(licenseKey)
-	sshCommand := fmt.Sprintf("sudo docker login -u license-token -p %s dev-registry.deckhouse.io", licenseKey)
-	//" && mkdir -p .ssh && sudo docker run --pull=always -t -v '/home/user/config.yml:/config.yml' dev-registry.deckhouse.io/sys/deckhouse-oss/install:main dhctl bootstrap --ssh-user=user --ssh-host=10.10.10.180 --ssh-password=user --config=/config.yml", licenseKey)
-
-	output, err = funcs.RemoteRun("user", "10.10.10.181", "user", sshCommand)
-	fmt.Printf("output: %v\n", output)
-	fmt.Printf("err: %v\n", err)
+	//output, err := funcs.RemoteRun("user", "10.10.10.181", "user", "ls -l /")
+	//fmt.Printf("output: %v\n", output)
+	//fmt.Printf("err: %v\n", err)
+	//
+	//output, err = funcs.RemoteRun("user", "10.10.10.181", "user", "sudo apt update && sudo apt -y install docker.io")
+	//fmt.Printf("output: %v\n", output)
+	//fmt.Printf("err: %v\n", err)
+	//
+	//licenseKey := os.Getenv("licensekey")
+	//fmt.Printf(licenseKey)
+	//sshCommand := fmt.Sprintf("sudo docker login -u license-token -p %s dev-registry.deckhouse.io", licenseKey)
+	////" && mkdir -p .ssh && sudo docker run --pull=always -t -v '/home/user/config.yml:/config.yml' dev-registry.deckhouse.io/sys/deckhouse-oss/install:main dhctl bootstrap --ssh-user=user --ssh-host=10.10.10.180 --ssh-password=user --config=/config.yml", licenseKey)
+	//
+	//output, err = funcs.RemoteRun("user", "10.10.10.181", "user", sshCommand)
+	//fmt.Printf("output: %v\n", output)
+	//fmt.Printf("err: %v\n", err)
 
 	err = funcs.CreateVM(ctx, cl, namespaceName, "vm1", "10.10.10.180", 4, "8Gi", "linstor-r1", "https://cloud-images.ubuntu.com/jammy/20240306/jammy-server-cloudimg-amd64.img", sshPubKeyString)
 	fmt.Printf("err: %v\n", err)
