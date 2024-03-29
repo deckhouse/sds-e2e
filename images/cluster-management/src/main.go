b131b3c81062a424766b98576cef78d8dd256314
v.oleynikov <vasily.oleynikov@flant.com>
2024-03-29 12:50:15 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/cluster-management/src/main.go
+++ images/cluster-management/src/main.go
@@ -128,7 +128,7 @@ func main() {
 			"sudo apt update && sudo apt -y install docker.io",
 			fmt.Sprintf("sudo docker login -u license-token -p %s dev-registry.deckhouse.io", licenseKey),
 			//			fmt.Sprintf("sudo mkdir -p /home/user/.ssh/ && sudo echo `%s` > /home/user/.ssh/id_rsa_test", sshPubKeyString),
-			"sudo docker run --pull=always -t -v '/home/user/config.yml:/config.yml' -v '/home/user/.ssh/:/tmp/.ssh/' dev-registry.deckhouse.io/sys/deckhouse-oss/install:main dhctl bootstrap --ssh-user=user --ssh-host=10.10.10.180 --ssh-agent-private-keys=/tmp/.ssh/id_rsa --config=/config.yml",
+			"sudo docker run --pull=always -t -v '/home/user/config.yml:/config.yml' -v '/home/user/.ssh/:/tmp/.ssh/' dev-registry.deckhouse.io/sys/deckhouse-oss/install:main dhctl bootstrap --ssh-user=user --ssh-host=10.10.10.180 --ssh-agent-private-keys=/tmp/.ssh/id_rsa_test --config=/config.yml",
 		} {
 			out, err := client.Run(sshCommand)
 			if err != nil {
