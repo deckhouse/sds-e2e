42a03c8ef9697786d7dbbc4b3bba17d40e598522
v.oleynikov <vasily.oleynikov@flant.com>
2024-05-15 11:17:56 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/sds-node-configurator-e2e/src/tests/01_lvm_volume_group_creation_test.go
+++ images/sds-node-configurator-e2e/src/tests/01_lvm_volume_group_creation_test.go
@@ -2,8 +2,17 @@ package test
 
 import (
 	"context"
+	"github.com/melbahja/goph"
+	"path/filepath"
 	"sds-node-configurator-e2e/funcs"
+	"strings"
 	"testing"
+)
+
+const (
+	appTmpPath = "/app/tmp"
+
+	privKeyName = "id_rsa_test"
 )
 
 func TestLvmVolumeGroupCreation(t *testing.T) {
@@ -20,5 +29,18 @@ func TestLvmVolumeGroupCreation(t *testing.T) {
 
 	for _, item := range devices {
 		t.Log(funcs.CreateLvmVolumeGroup(ctx, cl, item.Status.NodeName, []string{item.ObjectMeta.Name}))
+	}
+
+	for _, ip := range []string{"10.10.10.180", "10.10.10.181", "10.10.10.182"} {
+		auth, err := goph.Key(filepath.Join(appTmpPath, privKeyName), "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		client := funcs.GetSSHClient(ip, "user", auth)
+		defer client.Close()
+		out, err := client.Run("sudo vgdisplay -C")
+		if !strings.Contains(string(out), "data") || !strings.Contains(string(out), "20.00g") || err != nil {
+			t.Fatal("vgdisplay -C error", err)
+		}
 	}
 }
