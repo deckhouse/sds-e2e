bc4c56f82d41084f6c44e0b70de27ab211d4fc59
v.oleynikov <vasily.oleynikov@flant.com>
2024-06-27 16:15:41 +0300

    fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- testkit/clusterManagement/cluster.go
+++ testkit/clusterManagement/cluster.go
@@ -231,6 +231,7 @@ func InitClusterCreate() {
 	for strings.Contains(string(nodeInstallScript), "not found") {
 		log.Printf(nodeInstallGenerationCommand)
 		nodeInstallScript, err = masterClient.Run("whereis jq")
+		log.Printf(string(nodeInstallScript))
 		nodeInstallScript, err = masterClient.Run(nodeInstallGenerationCommand)
 		log.Printf(string(nodeInstallScript))
 		logFatalIfError(err, "")
