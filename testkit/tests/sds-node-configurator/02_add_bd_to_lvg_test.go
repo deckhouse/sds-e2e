cd80f3e5c1af9fa3afd0001ffbcc6abc7884dcca
v.oleynikov <vasily.oleynikov@flant.com>
2024-08-11 20:21:09 +0300

    Fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- testkit/tests/sds-node-configurator/02_add_bd_to_lvg_test.go
+++ testkit/tests/sds-node-configurator/02_add_bd_to_lvg_test.go
@@ -2,8 +2,12 @@ package sds_node_configurator
 
 import (
 	"context"
+	"fmt"
 	"github.com/deckhouse/sds-e2e/funcs"
+	"github.com/deckhouse/virtualization/api/core/v1alpha2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -17,6 +21,32 @@ func TestAddBDtoLVG(t *testing.T) {
 	extCl, err := funcs.NewKubeClient("")
 	if err != nil {
 		t.Error("Parent cluster kubeclient problem", err)
+	}
+
+	for _, vmName := range []string{"vm1", "vm2", "vm3"} {
+		vmdName := fmt.Sprintf("%s-data-add", vmName)
+		_, err = funcs.CreateVMD(ctx, extCl, funcs.NamespaceName, vmdName, funcs.StorageClass, 5)
+		if err != nil {
+			t.Error("Disk creation failed", err)
+		}
+
+		err = cl.Create(ctx, &v1alpha2.VirtualMachineBlockDeviceAttachment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      vmdName,
+				Namespace: funcs.NamespaceName,
+			},
+			Spec: v1alpha2.VirtualMachineBlockDeviceAttachmentSpec{
+				VirtualMachineName: vmName,
+				BlockDeviceRef: v1alpha2.VMBDAObjectRef{
+					Kind: "VirtualDisk",
+					Name: vmdName,
+				},
+			},
+		})
+		if err != nil && !strings.Contains(err.Error(), "already exists") {
+			t.Error("Disk attack failed", err)
+		}
+
 	}
 
 }
