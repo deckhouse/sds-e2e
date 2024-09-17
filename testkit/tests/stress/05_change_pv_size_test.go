481c9a269fe44b267f5affb3b397792066321150
v.oleynikov <vasily.oleynikov@flant.com>
2024-09-17 21:38:49 +0300

    Fix
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- testkit/tests/stress/05_change_pv_size_test.go
+++ testkit/tests/stress/05_change_pv_size_test.go
@@ -2,6 +2,7 @@ package stress
 
 import (
 	"context"
+	"fmt"
 	"github.com/deckhouse/sds-e2e/funcs"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -44,7 +45,11 @@ func TestChangeStsPvcSize(t *testing.T) {
 		allPvcChanged = true
 		for _, pvc := range pvcs.Items {
 			parse := resource.MustParse(pvResizedSize)
-			if parse.Cmp(*pvc.Status.AllocatedResources.Storage()) != 0 {
+			fmt.Println(*pvc.Status.Capacity.Storage())
+			fmt.Println(*pvc.Status.AllocatedResources.Storage())
+			fmt.Println(*pvc.Spec.Resources.Requests.Storage())
+			fmt.Println(resource.MustParse(pvResizedSize))
+			if parse.Cmp(*pvc.Status.Capacity.Storage()) != 0 {
 				allPvcChanged = false
 			}
 		}
